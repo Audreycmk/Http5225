d9893a38bb4d8a127e51e9200246bf0fb466fe97
Audrey <Audrey@AudreyMacBook-Air.local>
2025-01-21 18:29:44 -0500

    Week 2

---
+++ draft/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <!-- Trying out JS -->
+    <script> 
+    document.write('Hello World In JS');
+    </script>
+    
+    <!-- Trying out echo in PHP -->
+    <!-- \ esc treat as pure Eng -->
+    <?php
+      echo 'Welcome to Gary\'s World';
+      echo 'Another echo';
+    ?>
+</body>
+</html>
---
+++ week2/index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+  <head>
+    
+    <title>PHP and Creating Output</title>
+    
+  </head>
+  <body>
+  
+    <?php
+    echo '<h1>PHP and Creating Output</h1>';
+    echo '<p>The PHP echo command can be used to create output.</p>';
+    ?>
+
+    <p>When creating output using echo and PHP, quotes can often cause problems. There are several solutions to using quotes within an echo statement:</p>
+    
+    <ul>
+        <li>Use HTML special characters</li>
+        <li>Alternate between single and double quotes</li>
+        <li>Use a backslash to escape quotes</li>
+    </ul>
+    
+    <h2>More HTML to Convert</h2>
+
+    <p>PHP says "Hello World!"</p>
+
+    <p>Can you display a sentence with ''' and """?</p>
+
+    <img src="php-logo.png">
+
+    <?php
+      echo '<img src="http://google.com/image">';
+    ?>
+
+    <img src="<?php echo 'http://google.com/image' ?>" alt="<?php echo 'ALT TAG'; ?>">
+
+    <br><br><br>
+
+    <?php
+    $name = "Audrey";
+    $lastName = 'Chung';
+
+    echo "Hello, " . $name;
+
+    // $person = array('Audrey','Chung','audreycmk@gmail.com');
+    // $person[0];
+
+    $person['first'] = 'Audrey';
+    $person['last'] = 'Chung';
+    $person['email'] = 'audreycmk@gmail.com';
+    $person['web'] = 'http://audrey.com';
+
+    echo 'Hello, ' . $person['first'];
+
+    ?>
+
+    <a href="<?php echo 'mailto:audreycmk@gmail.com'; ?>">audreycmk@gmail.com</a>
+    <br>
+    <a href="<?php echo 'https://google.com';?>">Google</a>
+
+  </body>
+</html>
