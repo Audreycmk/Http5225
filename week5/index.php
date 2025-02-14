1f45b633d8763cf64f5ac547c97ac90e9a4aba45
Audrey <Audrey@AudreyMacBook-Air-2.local>
2025-02-13 20:48:31 -0500

    add week5_color

--- week5/index.php
+++ week5/index.php
@@ -14,13 +14,31 @@
       die('Connection Failed: ' . mysqli_connect_error());
     }
     
-    //table name
-    $query = "SELECT * FROM colors";
-    $colors = mysqli_query($connect, $query);
-    echo '<div style="background-color:' . $colors['Hex']. '"></div>'
-   
-  ?>
+     //table name
+     $query = "SELECT `Name`, `Hex` FROM colors";
+     $colors = mysqli_query($connect, $query);
+ 
+     if(mysqli_num_rows($colors) > 0){
+       while($row = mysqli_fetch_assoc($colors)){
+           echo '<div style="
+                   width:180px; 
+                   padding:10px; 
+                   margin:10px; 
+                   display:inline-block; 
+                   text-align:center; 
+                   font-family:Arial; 
+                   font-weight:bold; 
+                   color:#fff; 
+                   border-radius:8px; 
+                   box-shadow:2px 2px 5px rgba(0,0,0,0.2); 
+                   background-color:' . htmlspecialchars($row['Hex']) . ';">
+                 <h2>' . htmlspecialchars($row['Name']) . '</h2>
+                 </div>';
+     }
+   }
+   ?>
 </body>
 </html>
 <!-- test if mysqli is working -->
 <!-- echo '<pre>' . print_r($colors) . '</pre>' -->
+
